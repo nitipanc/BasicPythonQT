74cbe8f7c41808f6c85f18837f67c53ee7ae2be6
Nee_Panc <39270724+nitipanc@users.noreply.github.com>
2023-08-19 21:14:07 +0200

    Add files via upload

---
+++ BasicPythonQT/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='BasicPythonQT',
+    version='0.1.0',
+    author='Niti Panchal',
+    author_email='niti.panchal@gmail.com',
+    description='Create Dynamic Button',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
